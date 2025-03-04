73bde8c206c1b43259af1e760e2505a13643f850
rubences <91731294+rubences@users.noreply.github.com>
2025-03-04 09:11:46 +0000

    solucion_ejercicio1_fibo

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    
+    fib_sequence = [0, 1]
+    for i in range(2, n):
+        fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    return fib_sequence
+
+if __name__ == "__main__":
+    n = int(input("Enter the number of Fibonacci terms: "))
+    print(fibonacci(n))
